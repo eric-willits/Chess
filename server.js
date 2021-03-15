7a2419ff87b79a2970c4cd9e955a03bb0de1c2c5
Eric Willits <ericwillits@gmail.com>
2021-03-15 10:47:14 -0700

    fix dependencies

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.get('/ping', function (req, res) {
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.set("port", port);
+app.listen(port);
